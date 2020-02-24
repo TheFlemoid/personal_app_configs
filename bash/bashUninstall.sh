e1795f8905a0b3eb554357c45aed035c2419a847
DeliciousLunch55 <trey.dahlberg@gmail.com>
2020-02-24 13:57:44 -0600

    Quick fix after last commit.

--- bash/bashUninstall.sh
+++ bash/bashUninstall.sh
@@ -8,4 +8,7 @@ if [ -f "$FILE" ]; then
 	cp ~/.bashrc .bashTemp
 	sed -i '/# BEGIN DELICIOUSLUNCH55 APPENDED BASHRC/,/# END DELICIOUSLUNCH55 APPENDED BASHRC/d' .bashTemp
 	mv .bashTemp ~/.bashrc
+else
+	echo "No .bashrc config file found!"
+	echo "Nothing to do!"
 fi
